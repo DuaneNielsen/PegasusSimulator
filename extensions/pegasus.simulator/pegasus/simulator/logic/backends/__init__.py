41614f973c9952fdb1b8941b439597fc828b6709
duane <duane.nielsen.rocks@gmail.com>
2023-12-26 15:38:10 -0800

    Only load ROS2 Backend if ROS2 bridge is present

--- extensions/pegasus.simulator/pegasus/simulator/logic/backends/__init__.py
+++ extensions/pegasus.simulator/pegasus/simulator/logic/backends/__init__.py
@@ -3,6 +3,10 @@
 | License: BSD-3-Clause. Copyright (c) 2023, Marcelo Jacinto. All rights reserved.
 """
 
+from omni.isaac.core.utils.extensions import get_extension_id
+
 from .backend import Backend
 from .mavlink_backend import MavlinkBackend, MavlinkBackendConfig
-from .ros2_backend import ROS2Backend
+
+if get_extension_id("omni.isaac.ros2_bridge") is not None:
+    from .ros2_backend import ROS2Backend
